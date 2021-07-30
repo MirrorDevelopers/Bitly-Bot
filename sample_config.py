fa6be25f60c0d0535b8a758e7dcafffbc14ebf2e
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-07-30 22:11:15 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,3 +6,5 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 6))
 
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+
+    BITLY_TOKEN = os.environ.get("BITLY_TOKEN", "")
